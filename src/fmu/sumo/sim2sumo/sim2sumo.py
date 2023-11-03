4ae9af56982b77d386b00147bb4f964e84fe27c9
Daniel Berge Sollien <daniel.sollien@gmail.com>
2023-11-03 15:54:11 +0100

    Add overiding arguments in argsparse

--- src/fmu/sumo/sim2sumo/sim2sumo.py
+++ src/fmu/sumo/sim2sumo/sim2sumo.py
@@ -153,11 +153,12 @@ def export_results(
     return exp_path
 
 
-def read_config(config):
+def read_config(config, **kwargs):
     """Read config settings
 
     Args:
         config (dict): the settings for export of simulator results
+        kwargs (dict): overiding settings
 
     Returns:
         tuple: datafiles as list, submodules to use as list, and options as kwargs
@@ -208,6 +209,9 @@ def read_config(config):
         logger.info("No special options selected")
         options = {}
     options["arrow"] = options.get("arrow", True)
+    datafiles = kwargs.get("datafile", datafiles)
+    if "datatype" in kwargs:
+        submods = [kwargs["datatype"]]
     logger.info(
         "Running with: datafile(s): \n%s \n Types: \n %s \noptions:\n %s",
         datafiles,
@@ -217,20 +221,23 @@ def read_config(config):
     return datafiles, submods, options
 
 
-def export_with_config(config_path):
+def export_with_config(config_path, **extras):
     """Export several datatypes with yaml config file
 
     Args:
         config_path (str): path to existing yaml file
+        extras (dict): extra arguments
+
     """
     logger = logging.getLogger(__file__ + ".export_w_config")
+    logger.debug("Using extras %s", extras)
     suffixes = set()
     export_folder = None
     export_path = None
     try:
         count = 0
 
-        datafiles, submods, options = read_config(yaml_load(config_path))
+        datafiles, submods, options = read_config(yaml_load(config_path), **extras)
         for datafile in datafiles:
             for submod in submods:
                 logger.info("Exporting %s", submod)
@@ -313,6 +320,18 @@ def parse_args():
         help="Which sumo environment to upload to",
         default="prod",
     )
+    exec_parser.add_argument(
+        "--datatype",
+        type=str,
+        default=None,
+        help="Override datatype setting, intented for testing only",
+    )
+    exec_parser.add_argument(
+        "--datafile",
+        type=str,
+        default=None,
+        help="Override datafile setting, intented for testing only",
+    )
     help_parser.add_argument(
         "help_on",
         type=str,
@@ -365,18 +384,19 @@ def give_help(submod, only_general=False):
     return text_to_return
 
 
-def upload_with_config(config_path, env="prod"):
+def upload_with_config(config_path, env="prod", **extras):
     """Upload simulator results to sumo
 
     Args:
         config_path (str): Path to config file
         env (str, optional): The sumo environment. Defaults to "prod".
+        extras (dict): extra arguments
     """
     logger = logging.getLogger(__file__ + ".upload_with_config")
     logger.debug("Executing with:")
     logger.debug("config: %s: ", config_path)
     logger.debug("Sumo env: %s: ", env)
-    upload_folder, suffixes = export_with_config(config_path)
+    upload_folder, suffixes = export_with_config(config_path, **extras)
     upload(upload_folder, suffixes, env)
 
 
@@ -384,11 +404,16 @@ def main():
     """Main function to be called"""
     logger = logging.getLogger(__file__ + ".main")
     args = parse_args()
+    extras = {
+        key: value
+        for key, value in vars(args).items()
+        if key in ["datafile", "datatype"] and value is not None
+    }
     logger.debug(vars(args))
     try:
         print(give_help(args.help_on))
     except AttributeError:
-        upload_with_config(args.config_path, args.env)
+        upload_with_config(args.config_path, args.env, **extras)
 
 
 if __name__ == "__main__":
