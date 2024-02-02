1585803a595d47ef52c13471e3cf722874db3622
Daniel Sollien <daniel.sollien@gmail.com>
2024-02-02 15:22:00 +0100

    Add to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,139 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+#!/usr/bin/env python3
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# flake8: noqa
+# pylint: skip-file
+import os
+import sys
+from pathlib import Path
+from datetime import date
 
-project = 'fmu-sumo-sim2sumo'
-copyright = '2024, Daniel Berge Sollien'
-author = 'Daniel Berge Sollien'
+# directly inject into sys.path the path where all modules are installed
+sys.path.insert(0, os.path.abspath(str(Path(__file__).parent.parent / "src")))
+print(sys.path[0])
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import fmu.sumo.sim2sumo as sim2sumo
 
-extensions = []
+# from fmu.sumo import explorer, uploader
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+# -- General configuration ---------------------------------------------
 
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# The full version, including alpha/beta/rc tags.
+# release = fmu.sumo.sim2sumo.__version__
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+extensions = [
+    "sphinxcontrib.apidoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+]
+
+autodoc_mock_imports = ["sim2sumo"]
+
+os.environ["SPHINX_APIDOC_OPTIONS"] = (
+    "members,show-inheritance,inherited-members"
+)
+# autoapi_modules: dict = {"sim2sumo": None}
+apidoc_module_dir = "../src/fmu/sumo/sim2sumo/"
+
+apidoc_output_dir = "apiref"
+apidoc_excluded_paths = [
+    "version.py",
+    "hook_implementations/*.py",
+    "config_jobs/*.py",
+]
+apidoc_separate_modules = True
+apidoc_module_first = True
+apidoc_extra_args = ["-H", "API reference for fmu.sumo.sim2sumo"]
+
+autoclass_content = "both"
+
+napoleon_include_special_with_doc = False
+
+# The suffix of source filenames.
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
+
+# The master toctree document.
+master_doc = "index"
+
+# General information about the project.
+project = "fmu.sumo.sim2sumo"
+current_year = date.today().year
+# copyright = "Equinor " + str(current_year) + f" (fmu-sumo-sim2sumo release {release})"
+
+
+# Sort members by input order in classes
+autodoc_member_order = "bysource"
+autodoc_default_flags = ["members", "show-inheritance", "inherited-members"]
+
+exclude_patterns = ["_build"]
+
+pygments_style = "sphinx"
+
+html_theme = "sphinx_rtd_theme"
+
+html_logo = "_static/equinor-logo2.jpg"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# html_theme_options = {
+#     "style_nav_header_background": "#C0C0C0",
+# }
+
+
+# html_logo = "images/xtgeo-logo.png"
+
+# The name of an image file (within the static path) to use as favicon
+# of the docs.  This file should be a Windows icon file (.ico) being
+# 16x16 or 32x32 pixels large.
+# html_favicon = None
+
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page
+# bottom, using the given strftime format.
+# html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+# html_use_smartypants = True
+
+# Custom sidebar templates, maps document names to template names.
+# html_sidebars = {}
+
+# Additional templates that should be rendered to pages, maps page names
+# to template names.
+# html_additional_pages = {}
+
+# If false, no module index is generated.
+# html_domain_indices = True
+
+# If false, no index is generated.
+# html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+# html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+# html_show_sourcelink = True
+
+# If true, "Created using Sphinx" is shown in the HTML footer.
+# Default is True.
+# html_show_sphinx = True
+
+# If true, "(C) Copyright ..." is shown in the HTML footer.
+# Default is True.
+# html_show_copyright = True
+
+# If true, an OpenSearch description file will be output, and all pages
+# will contain a <link> tag referring to it.  The value of this option
+# must be the base URL from which the finished HTML is served.
+# html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. ".xhtml").
+# html_file_suffix = None
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "fmu-sumo-sim2sumo"
