475494e386abfe415650fe145746637d62f6b675
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-09-09 09:07:39 +0200

    Remove troll specific test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -243,65 +243,6 @@ def test_prepare_for_sendoff(config, nrdatafiles, nrsubmodules, tmp_path):
         ), f"{subdict} for {submod} expected to have {nrsubmodules} submodules"
 
 
-def test_find_datafile_paths(tmp_path):
-    _ = create_troll_case(tmp_path)
-    inputs = find_datafile_paths()
-    print(inputs)
-
-
-def test_prepare_for_sendoff_troll_case(tmp_path):
-
-    expected_datafile_nr = 2
-    expected_troll_pred_input = {
-        "pvt": {"keywords": ["PVTO", "PVDG"], "arrow": True},
-        "grid3d": False,
-    }
-
-    ix_file = create_troll_case(tmp_path)
-    config = {
-        "sim2sumo": {
-            "datafile": {
-                "ix/model/TROLL_PRED_IX-1": {"summary": {"column_keys": "*"}},
-                "eclipse/migrator/TROLL_PRED_IX-1": {
-                    "pvt": {"keywords": ["PVTO", "PVDG"]}
-                },
-                "ix/include/ENS_NETWORK": {"vfp": {"keyword": ["VFPPROD"]}},
-            }
-        }
-    }
-    inputs = prepare_for_sendoff(config)
-    print(inputs)
-    assert (
-        len(inputs) == expected_datafile_nr
-    ), f"Expected to extract {expected_datafile_nr} datafiles"
-
-    troll_pred_input = inputs[ix_file]
-
-    assert (
-        troll_pred_input == expected_troll_pred_input
-    ), f"Expected to extract {expected_troll_pred_input}, but found {troll_pred_input}"
-
-
-def create_troll_case(tmp_path):
-    real1 = tmp_path / "realone"
-    copytree(REEK_REAL1, real1)
-
-    troll_pred_name = "TROLL_PRED_IX-1.afi"
-    ix_file = real1 / "ix/model" / troll_pred_name
-    mig_folder = real1 / "eclipse/migrator"
-    mig_folder.mkdir(parents=True)
-    inc_folder = real1 / "ix/include/"
-    inc_folder.mkdir(parents=True)
-    inc_file = inc_folder / "ENS_NETWORK.afi"
-    mig_file = mig_folder / "TROLL_PRED_IX-1.afi"
-    mig_file.write_text("hei")
-    ix_file.write_text("hei")
-    inc_file.write_text("hei")
-
-    os.chdir(real1)
-    return mig_file
-
-
 def test_Dispatcher(case_uuid, token, scratch_files, monkeypatch):
     disp = Dispatcher(scratch_files[2], "dev", token=token)
     monkeypatch.chdir(scratch_files[0])
