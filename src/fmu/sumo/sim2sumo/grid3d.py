2a9a8b0c57171d7a9bc80e9c382659712b2f0808
Runar Ask Johannessen <ruaj@equinor.com>
2024-09-13 09:45:11 +0200

    Use Path.with_suffix() instead of custom function

--- src/fmu/sumo/sim2sumo/grid3d.py
+++ src/fmu/sumo/sim2sumo/grid3d.py
@@ -6,6 +6,7 @@
    3. Uploads to Sumo
 """
 import logging
+from pathlib import Path
 import re
 from datetime import datetime
 
@@ -21,7 +22,6 @@ from .common import (
     generate_meta,
     convert_to_bytestring,
     convert_2_sumo_file,
-    fix_suffix,
 )
 
 
@@ -355,9 +355,10 @@ def upload_simulation_run(datafile, config, dispatcher):
         datafile (str): path to datafile
     """
     logger = logging.getLogger(__name__ + ".upload_simulation_run")
-    init_path = fix_suffix(datafile, ".INIT")
-    restart_path = fix_suffix(datafile, ".UNRST")
-    grid_path = fix_suffix(datafile, ".EGRID")
+    datafile_path = Path(datafile)
+    init_path = datafile_path.with_suffix(".INIT")
+    restart_path = datafile_path.with_suffix(".UNRST")
+    grid_path = datafile_path.with_suffix(".EGRID")
     egrid = Grid(grid_path)
     xtgeoegrid = grid_from_file(grid_path)
     # grid_exp_path = export_object(
