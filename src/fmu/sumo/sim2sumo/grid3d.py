222676a91da2ac53c77c5efa9574d7fd1098086a
Runar Ask Johannessen <89020325+equinor-ruaj@users.noreply.github.com>
2025-01-23 13:25:06 +0100

    Debug printing

--- src/fmu/sumo/sim2sumo/grid3d.py
+++ src/fmu/sumo/sim2sumo/grid3d.py
@@ -81,12 +81,8 @@ def generate_grid3d_meta(datafile, obj, config):
     metadata_path = datafile_path.parent / f".{datafile_path.name}.yml"
     metadata = yaml_load(metadata_path)
 
-    relative_parent = str(Path(datafile).parents[2]).replace(
-        str(Path(datafile).parents[4]), ""
-    )
-    metadata["file"]["relative_path"] = (
-        f"{relative_parent}/{tagname}--{name}".lower()
-    )
+    print(f"METADATA FILE GRID3d: {metadata['file']}")
+    print("----------")
     assert isinstance(metadata, dict), (
         f"meta should be dict, but is {type(metadata)}"
     )
@@ -123,12 +119,15 @@ def generate_gridproperty_meta(datafile, obj, prefix, config, geogrid):
 
     exd = ExportData(**exp_args)
     metadata = exd.generate_metadata(obj)
+    print(f"METADATA FILE grid_property BEFORE CHANGE: {metadata['file']}")
     relative_parent = str(Path(datafile).parents[2]).replace(
         str(Path(datafile).parents[4]), ""
     )
     metadata["file"]["relative_path"] = (
         f"{relative_parent}/{tagname}--{name}".lower()
     )
+    print(f"METADATA FILE grid_property AFTER CHANGE: {metadata['file']}")
+    print("----------")
     assert isinstance(metadata, dict), (
         f"meta should be dict, but is {type(metadata)}"
     )
