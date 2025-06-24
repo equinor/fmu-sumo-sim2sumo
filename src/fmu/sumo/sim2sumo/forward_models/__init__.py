60252ed7a230836007fa348a1289598f920e01be
Runar Ask Johannessen <89020325+equinor-ruaj@users.noreply.github.com>
2025-06-24 14:08:21 +0200

    feat: use Erts newer plugin system (#157)
    * chore: silence type checker on ert import
    
    * feat: update doc text
    
    * feat: add description to plugin
    
    * feat: remove old job definitions

--- src/fmu/sumo/sim2sumo/forward_models/__init__.py
+++ src/fmu/sumo/sim2sumo/forward_models/__init__.py
@@ -1,10 +1,13 @@
 import subprocess
 
-from ert import (
+from ert import (  # type: ignore
+    ForwardModelStepDocumentation,
     ForwardModelStepJSON,
     ForwardModelStepPlugin,
     ForwardModelStepValidationError,
+    plugin,
 )
+from ert.plugins.plugin_manager import hook_implementation
 
 
 class Sim2Sumo(ForwardModelStepPlugin):
@@ -46,3 +49,62 @@ class Sim2Sumo(ForwardModelStepPlugin):
 
         if return_code != 0:
             raise ForwardModelStepValidationError(err_msg)
+
+    @staticmethod
+    def documentation() -> ForwardModelStepDocumentation | None:
+        return ForwardModelStepDocumentation(
+            description="""
+Makes result simulator (Eclipse, OPM, IX) available in sumo.
+This is done by SIM2SUMO in a three step process:
+1. Data is extracted in arrow format using res2df
+2. Corresponding metadata are generated via fmu-dataio
+3. Data and metadata are then uploaded to Sumo using fmu-sumo-upload
+SIM2SUMO defaults to extract summary, rft and well completion data, but
+you can configure it to extract any datatype that res2df can produce.
+
+SIM2SUMO finds results from the eclipse/model/ folder in your realization.
+It looks for files with suffix .DATA, and then finds the eclipse run results.
+This means that if you have more than one eclipse run in that folder, you will
+get the specified result from all those runs.
+The results are stored in Sumo with name <name of datafile> without the
+habituary -<IENS> at the end.
+Tagname will be name of datatype extracted.
+
+E.G: summary files extracted from an eclipse run named DROGON-1, will be
+stored in Sumo with name: DROGON, tagname: summary, and fmu.realization.id: 1
+
+ Pre-requisites: SIM2SUMO is dependent on a configuration yaml file.
+                (defaulted to ../../fmuconfig/output/global_variables.yml).
+                This file needs to contain masterdata, and if you want
+                to have some custom configurations, this is also done here .
+                See examples.
+
+""",
+            examples="""
+1. Extracting the defaults, with fmu_config in the standard location:
+
+   In an Ert config file
+   FORWARD_MODEL ECLIPSE100(...)
+   --Note: SIM2SUMO must come after ECLIPSE100
+   FORWARD_MODEL SIM2SUMO(<S2S_CONFIG_PATH>=path/to/config/file)
+
+2. Extracting the defaults, but with fmu_config in a non standard location:
+
+   In an Ert config file
+   FORWARD_MODEL ECLIPSE100(...)
+   --Note: SIM2SUMO must come after ECLIPSE100
+   FORWARD_MODEL SIM2SUMO(<S2S_CONFIG_PATH>=path/to/config/file)
+
+3. When you want to configure the fmu_config file to control what SIM2SUMO
+   produces add section sim2sumo to your fmu_config file.
+   (for inclusion of the forward model in your ert setup see examples 1. or 2.)
+""",
+            category="export",
+            source_package="sim2sumo",
+        )
+
+
+@hook_implementation
+@plugin(name="fmu_sumo_sim2sumo")
+def installable_forward_model_steps():
+    return [Sim2Sumo]
